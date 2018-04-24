73994cc012b299defffce9e8cb55f598af0cd8aa
Milos Stojanovic <stojanovicmilos92@outlook.com>
2018-04-24 22:44:32 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const parts = require("./webpack.parts");
+
+const commonConfig = merge([
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Webpack demo",
+            }),
+        ],
+    },
+]);
+
+const productionConfig = merge([
+    parts.extractCSS({
+        use: "css-loader",
+    }),
+]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+    parts.loadCSS(),
+]);
+
+module.exports = mode => {
+    if (mode === "production") {
+        return merge(commonConfig, productionConfig, { mode });
+    }
+    return merge(commonConfig, developmentConfig, { mode });
+};
---
+++ webpack.parts.js
@@ -0,0 +1,52 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+exports.extractCSS = ({ include, exclude, use }) => {
+    // Output extracted CSS to a file
+    const plugin = new ExtractTextPlugin({
+        // `allChunks` is needed to extract from extracted chunks as well.
+        allChunks: true,
+        filename: "[name].css",
+    });
+
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    include,
+                    exclude,
+
+                    use: plugin.extract({
+                        use,
+                        fallback: "style-loader",
+                    }),
+                },
+            ],
+        },
+        plugins: [plugin],
+    };
+};
+
+exports.devServer = ({ host, port } = {}) => ({
+    devServer: {
+        stats: "errors-only",
+        host, // Defaults to `localhost`
+        port, // Defaults to 8080
+        open: true,
+        overlay: true,
+    },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                include,
+                exclude,
+
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+});
